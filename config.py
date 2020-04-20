7ddacf53d56e6cb41a555f5d61e33cadbc0834b7
regnartsss <4Kikbtyma>
2020-04-20 18:14:28 +0700

    v3

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ TOKEN = '1225386946:AAEzxslBD9BNeEnS_aoyfgsgnfiBD55hBtA'
 AD_USER = 'podkopaev.k@partner.ru'
 AD_PASSWORD = 'z15X3vdy'
 #test
-print("ts")
+print("ts12312312")
 try:
     apihelper.proxy = {'https': 'socks5://SsBNpW:oTUn9X@194.242.126.235:8000'}
 #    apihelper.proxy = {'https': 'socks5://UFyToM:6vwN31X@185.233.83.61:9300'}
