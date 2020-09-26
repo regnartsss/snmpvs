0d722826c75cb9044227cabca9a0d14e86fc3a3b
reg <regnartsss@gmail.com>
2020-09-26 10:59:10 +0700

    new_snmp

--- work/check_registrator.py
+++ work/check_registrator.py
@@ -45,7 +45,7 @@ trassirmonitoring = ['1.3.6.1.4.1.3333.1.3', '1.3.6.1.4.1.3333.1.5', '1.3.6.1.4.
 async def snmpregist(ip):
     d = []
     for r in trassirmonitoring:
-        with aiosnmp.Snmp(host=ip, port=161, community="dssl", timeout=10, retries=3, max_repetitions=5, ) as snmp:
+        with aiosnmp.Snmp(host=ip, port=161, community="dssl", timeout=15, retries=3, max_repetitions=7, ) as snmp:
             try:
                 for res in await snmp.get(r):
                     if r == '1.3.6.1.4.1.3333.1.12':
