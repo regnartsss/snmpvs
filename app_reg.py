74d82eb03b7e9b99fb6e5362bd97b17df8b2cfb3
regnartsss <4Kikbtyma>
2020-10-30 07:31:59 +0700

    new_version_v1

---
+++ app_reg.py
@@ -0,0 +1,34 @@
+from work.zabbix_check import check
+from work.zabbix_check_cisco import start_snmp, start_snmp_operstatus
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from loader import bot
+from work.zabbix_check_registrator import shed, start_check_registrator, start_check_registrator_cam
+from loader import dp
+import asyncio
+import middlewares
+from work.test import test
+middlewares.setup(dp)
+
+
+async def zabb():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(check, 'interval', hours=4)
+    scheduler.start()
+
+
+def on_startup():
+    # bot.send_message(765333440, "Бот запущен")
+    # loop = asyncio.ProactorEventLoop()
+    # asyncio.set_event_loop(loop)
+
+    loop = asyncio.get_event_loop()
+    # asyncio.ensure_future(start_snmp("ASC"))
+    # asyncio.ensure_future(zabb())
+    # asyncio.ensure_future(check())
+    asyncio.ensure_future(start_check_registrator())
+    # asyncio.ensure_future(test())
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    on_startup()
